287abe4eeb814ee48bb304fa4ab331663a2e93df
Joshi <joshua.sauter49@gmx.de>
2026-08-28 12:53:31 +0200

    feat: Titel

--- src/main/java/org/schachlernapp/ui/MainMenuView.java
+++ src/main/java/org/schachlernapp/ui/MainMenuView.java
@@ -3,7 +3,10 @@ package org.schachlernapp.ui;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 /**
@@ -19,7 +22,7 @@ import javafx.scene.layout.VBox;
  * "Zughinweis anzeigen"-Schalter des Eröffnungstrainers sitzt bewusst NICHT hier, sondern beim
  * Brett in {@link org.schachlernapp.ui.opening.OpeningPanel}. Unten schließt ein
  * "Programm beenden"-Button die App (löst regulär {@code Main.stop()} aus - Fortschritt wird
- * gespeichert).</p>
+ * gespeichert). Oben steht der Titel "Joshi's Chess Tutor" mit einer Dame (weißes cburnett-Bild).</p>
  */
 public class MainMenuView extends VBox {
 
@@ -28,8 +31,20 @@ public class MainMenuView extends VBox {
         setSpacing(8);
         setPadding(new Insets(8));
         setAlignment(Pos.CENTER);
-        setPrefWidth(200);
+        setPrefWidth(240);
         getStyleClass().addAll("side-panel", "menu-pane");
+
+        ImageView queen = new ImageView(new Image(getClass().getResourceAsStream("/pieces/wQ.png")));
+        queen.setFitWidth(64);
+        queen.setFitHeight(64);
+        queen.setPreserveRatio(true);
+        queen.setSmooth(true);
+
+        Label titleLabel = new Label("Joshi's Chess Tutor");
+        titleLabel.getStyleClass().add("menu-title");
+
+        VBox header = new VBox(6, queen, titleLabel);
+        header.setAlignment(Pos.CENTER);
 
         Button newGameButton = new Button("Neues Spiel");
         Button newPuzzleButton = new Button("Neues Puzzle");
@@ -50,7 +65,8 @@ public class MainMenuView extends VBox {
         importGameButton.setOnAction(e -> onImportGame.run());
         quitButton.setOnAction(e -> onQuit.run());
 
-        getChildren().addAll(newGameButton, newPuzzleButton, openingButton, endgameButton, importGameButton,
+        getChildren().addAll(header, new Separator(),
+                newGameButton, newPuzzleButton, openingButton, endgameButton, importGameButton,
                 new Separator(), quitButton);
     }
 }
